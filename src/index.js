e5bed5c2107da5474e46f7eea12e069e1277d2ee
Marat Maksumov <maksumov@users.noreply.github.com>
2021-02-15 04:41:41 +0300

    refactor: make it workable on node12

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-exports.min = (array) => array?.sort((a, b) => a - b)[0] || 0;
+exports.min = (array) => (array && array.sort((a, b) => a - b)[0]) || 0;
 
-exports.max = (array) => array?.sort((a, b) => b - a)[0] || 0;
+exports.max = (array) => (array && array.sort((a, b) => b - a)[0]) || 0;
 
 exports.avg = (array) =>
-    array?.reduce((a, c) => (a += c), 0) / array?.length || 0;
+    (array && array.reduce((a, c) => (a += c), 0) / array.length) || 0;
